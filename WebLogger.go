bf2fd30a75c72491b9bf8d8816179df6d29dfc29
Shaq <Shaq@mail.ru>
2017-12-13 11:47:30 +1000

    q

--- WebLogger.go
+++ WebLogger.go
@@ -12,7 +12,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const DB_CONNECT_STRING = "host=localhost port=5432 user=postgres password=Mm000000 dbname=templogger sslmode=disable"
+const DB_CONNECT_STRING = "host=localhost port=5432 user=postgres password=Mm000000 dbname=logger sslmode=disable"
 const DB_CONNECT_STRING_INIT = "host=localhost port=5432 user=postgres password=Mm000000 dbname=postgres sslmode=disable"
 
 func main() {
