859e3a30133517e8ad34c163268903dbfa7df20c
Shaq <Shaq@mail.ru>
2017-12-15 11:05:21 +1000

    h

--- WebLogger.go
+++ WebLogger.go
@@ -417,7 +417,7 @@ func get_last_data(pdb **sql.DB, device_name string) string {
 		fmt.Println("Error query last data: ", err)
 		return ""
 	}
-	queryText = fmt.Sprintf("SELECT parameter_name,value FROM log_data WHERE event_time_id=%n", id)
+	queryText = fmt.Sprintf("SELECT parameter_name,value FROM log_data WHERE event_time_id=%d", id)
 	fmt.Println(queryText)
 	rows, err := db.Query(queryText)
 	if err != nil {
