a6e82cc23b58ffab94cb377d2484efc4646eb490
Shaq <Shaq@mail.ru>
2017-12-21 18:14:32 +1000

    Signed-off-by: Shaq <Shaq@mail.ru>

--- WebLogger.go
+++ WebLogger.go
@@ -542,7 +542,7 @@ func get_data_array(pdb **sql.DB, device_name string, datetime1 string, datetime
 	}
 	node := RespNode{data: data, time: prev_event_time}
 	//nodeArray = append(nodeArray, node)
-
+	fmt.Println("%s; %f; %f; %f; %f", node.time, node.data[0], node.data[1], node.data[2], node.data[3])
 	//	respStruct := RespStruct{parameters: parametersArray, data: nodeArray}
 	resJSON, err := json.Marshal(node) //respStruct)
 	if err != nil {
